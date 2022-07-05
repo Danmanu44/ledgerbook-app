c5aeca9634bc377913387459278c49369f5d21cd
Danmanu44 <53683648+Danmanu44@users.noreply.github.com>
2022-07-05 07:02:44 -0700

    changing Json server  url from local to live

--- src/api/client.js
+++ src/api/client.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 //change the base url when dealing with online server;
 // baseURL:"https://truthful-wind-yumberry.glitch.me/"
+//local :
+// baseURL:"http://localhost:3006/"
 export default axios.create({
-    baseURL:"http://localhost:3006/"
-
+   
+    baseURL:"https://truthful-wind-yumberry.glitch.me/"
 
 })
